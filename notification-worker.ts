e0687cdfc7e261cd02c98e8ec84d790c66c59e07
Ben Cuff <bencuff14@gmail.com>
2025-07-15 13:28:35 -0700

    feat: add expiration time for global notifications and ID

--- notification-worker.ts
+++ notification-worker.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import { createClient } from "redis";
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 
@@ -25,21 +26,24 @@ await sub.connect();
 await store.connect();
 
 const MAX_NOTIFICATIONS_TO_KEEP = 5;
+const GLOBAL_EXPIRE_SECONDS = 60 * 60 * 24; // 1 day
 
 await sub.subscribe("notifications", async (message) => {
   const { userId, text, type } = JSON.parse(message);
-  const notification = { text, type };
 
+  const notificationGlobal = { text, type, id: uuidv4() };
   if (type === "GLOBAL") {
     const globalKey = `notifications:global`;
-    await store.lPush(globalKey, JSON.stringify(notification));
+    await store.lPush(globalKey, JSON.stringify(notificationGlobal));
     await store.lTrim(globalKey, 0, MAX_NOTIFICATIONS_TO_KEEP - 1);
+    await store.expire(globalKey, GLOBAL_EXPIRE_SECONDS);
     return;
   }
 
+  const notificationUser = { text, type };
   if (userId) {
     const userKey = `notifications:${userId}`;
-    await store.lPush(userKey, JSON.stringify(notification));
+    await store.lPush(userKey, JSON.stringify(notificationUser));
     await store.lTrim(userKey, 0, MAX_NOTIFICATIONS_TO_KEEP - 1);
     return;
   }
